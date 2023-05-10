de10030d1bb1afedaf143e564758ce8887ac660e
b-sachin <sachinbhopiofficial@gmail.com>
2023-05-10 21:15:17 +0530

    added names file

--- sample.py
+++ sample.py
@@ -7,3 +7,15 @@ print(add(2,3))
 
 with open("greeting.txt", "r") as f:
     print(f.read())
+
+msg_file = open("greeting.txt","r")
+msg = msg_file.read()
+
+nms = open("names.txt","r")
+lines = nms.readlines()
+
+for line in lines:
+    print("{} {}".format(msg,line.strip()))
+
+msg_file.close()
+nms.close()
